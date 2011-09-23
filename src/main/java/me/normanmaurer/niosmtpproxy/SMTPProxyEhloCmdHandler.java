eb4e9232f7e3ab2b648fc656534161645966e0a7
norman <norman@apache.org>
2011-09-23 11:44:01 +0200

    Make sure we remove STARTTLS from the supported EXTENSIONS when proxy EHLO requests/responses

--- src/main/java/me/normanmaurer/niosmtpproxy/SMTPProxyEhloCmdHandler.java
+++ src/main/java/me/normanmaurer/niosmtpproxy/SMTPProxyEhloCmdHandler.java
@@ -1,6 +1,8 @@
 package me.normanmaurer.niosmtpproxy;
 
+import me.normanmaurer.niosmtp.SMTPClientConstants;
 import me.normanmaurer.niosmtp.core.SMTPRequestImpl;
+import me.normanmaurer.niosmtp.core.SMTPResponseImpl;
 import me.normanmaurer.niosmtp.transport.SMTPClientSession;
 
 import org.apache.james.protocols.smtp.SMTPResponse;
@@ -21,6 +23,18 @@ public class SMTPProxyEhloCmdHandler extends EhloCmdHandler implements SMTPProxy
             SMTPClientSession clientSession = (SMTPClientSession) session.getConnectionState().get(SMTP_CLIENT_SESSION_KEY);
             final String heloName = (String) session.getState().get(SMTPSession.CURRENT_HELO_NAME);
             clientSession.send(SMTPRequestImpl.ehlo(heloName), new ExtensibleSMTPProxyResponseCallback(session, futureResponse){
+
+                @Override
+                public void onResponse(SMTPClientSession clientSession, me.normanmaurer.niosmtp.SMTPResponse serverResponse) {
+                    SMTPResponseImpl copiedResponse = new SMTPResponseImpl(serverResponse.getCode());
+                    for (String responseLine: serverResponse.getLines()) {
+                        // remove announced STARTTLS
+                        if (!responseLine.equalsIgnoreCase(SMTPClientConstants.STARTTLS_EXTENSION)) {
+                            copiedResponse.addLine(responseLine);
+                        }
+                    }
+                    super.onResponse(clientSession, copiedResponse);
+                }
 
                 @Override
                 protected void onFailure(SMTPSession session, SMTPClientSession clientSession) {
